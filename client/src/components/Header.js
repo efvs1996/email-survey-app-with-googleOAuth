455268f4ac38e26020b2135d620a2f652c089660
Eliecer <eliecer@MacBook-Pro-de-Eliecer.local>
2021-05-26 19:17:21 -0600

    Added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent(){
@@ -8,9 +9,13 @@ class Header extends Component {
             case null:
                 return;
             case false: 
-                return (console.log(this.props),<li><a href="/auth/google">Login with google</a></li>);
+                return <li><a href="/auth/google">Login with google</a></li>;
             default: 
-                return (console.log(this.props.auth),<li><a href="/api/logout">Logout</a></li>);
+                return [
+                    <li key="1"><Payments/></li>,
+                    <li style={{margin: "0 10px"}}>Credits: {this.props.auth.credits}</li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const { mongoURI, cookieKey } = require('./config/keys');
 require('./models/User');
 require('./services/passport');
+
 
 
 mongoose.connect(mongoURI, {
@@ -15,6 +17,7 @@ mongoose.connect(mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.json())
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -26,6 +29,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index'))
+    });
+}
  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
